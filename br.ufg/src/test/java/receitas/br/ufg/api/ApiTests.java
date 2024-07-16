0ad7e4181e526d9a27ce600edfcb5d5f39a287ec
alexoliveiramartins <alex.oliveiramartins05@gmail.com>
2024-07-15 23:43:36 -0300

    .

--- br.ufg/src/test/java/receitas/br/ufg/api/ApiTests.java
+++ br.ufg/src/test/java/receitas/br/ufg/api/ApiTests.java
@@ -29,35 +29,5 @@ public class ApiTests {
 
     @Test
     void testReceitasIngredientes() throws Exception {
-        ArrayList<Receita> receitas = new ArrayList<>();
-        Receita receita = new Receita();
-        receita.setNome("Bolo de Chocolate");
-
-        ArrayList<Ingrediente> ingredientesArray = new ArrayList<>();
-        Ingrediente ingrediente1 = new Ingrediente();
-        ingrediente1.setNome("chocolate");
-        ingrediente1.setQuantidade("200g");
-
-        Ingrediente ingrediente2 = new Ingrediente();
-        ingrediente2.setNome("farinha");
-        ingrediente2.setQuantidade("300g");
-
-        ingredientesArray.add(ingrediente1);
-        ingredientesArray.add(ingrediente2);
-
-        receita.setIngredientes(ingredientesArray);
-        receitas.add(receita);
-
-        ArrayList<String> ingredientes = new ArrayList<>();
-        ingredientes.add("chocolate");
-        ingredientes.add("farinha");
-
-        when(receitaService.getReceitasByIngredients(ingredientes)).thenReturn(receitas);
-
-        mockMvc.perform(get("/api/receitas")
-                        .param("ingredientes", "chocolate")
-                        .param("ingredientes", "farinha"))
-                .andExpect(status().isOk())
-                .andExpect(content().json("[{'nome':'Bolo de Chocolate', 'ingredientes':[{'nome':'chocolate', 'quantidade':'200g'}, {'nome':'farinha', 'quantidade':'300g'}]}]"));
     }
 }
